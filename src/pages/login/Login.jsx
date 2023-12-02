b328f00cf4ae5b0b4bdce70dfa10433b8fa547d6
n9507246 <n9507246@gmail.com>
2023-12-03 00:28:35 +0300

    login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -3,8 +3,25 @@ import './login.scss'
 function Login() {
 
     return (
-      <div >
-        Login
+      <div className='login'>
+        <div className="card">
+          <div className="left">
+            <h1>Hello word</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae impedit rem repellat ea modi dolorum sequi deserunt nisi minus ipsa distinctio consequuntur, odio ullam eaque iusto dolores veritatis numquam. Reprehenderit!
+            </p>
+            <span>Don't you have an account?</span>
+            <button>Register</button>
+          </div>
+          <div className="right">
+            <h1>Login</h1>
+            <form>
+              <input type="text" placeholder='Username' />
+              <input type="text" placeholder='Username' />
+              <button>Login</button>
+            </form>
+          </div>
+        </div>
       </div>
     )
   }
