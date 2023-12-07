0bb6a8e0341a228071d8cf8512251bb729d7be2a
n9507246 <n9507246@gmail.com>
2023-12-07 21:03:42 +0300

    right bar

--- src/components/rightBar/RightBar.jsx
+++ src/components/rightBar/RightBar.jsx
@@ -2,6 +2,142 @@ import clasess from './styles.module.scss'
 
 export default function (){
     return(
-        <div className={clasess.rigthBar} >RightBar</div>
+        <div className={clasess.rigthBar} >
+            <div className={clasess.container}>
+                <div className={clasess.item}>
+                    <span>Suggestions For You</span>
+                    <div className={clasess.user}>
+                        <div className={clasess.userInfo}>
+                            <img src='https://huivpizde.com/uploads/posts/2022-12/1670018898_16-huivpizde-com-p-erotika-s-milfami-v-bele-18.jpg' alt=''/>
+                            <span>Jane Doe</span>
+                        </div>
+                        <div className={clasess.buttons}>
+                            <button>follow</button>
+                            <button>dismiss</button>
+                        </div>
+                    </div>
+                    <div className={clasess.user}>
+                        <div className={clasess.userInfo}>
+                            <img src='https://huivpizde.com/uploads/posts/2022-12/1670018898_16-huivpizde-com-p-erotika-s-milfami-v-bele-18.jpg' alt=''/>
+                            <span>Jane Doe</span>
+                        </div>
+                        <div className={clasess.buttons}>
+                            <button>follow</button>
+                            <button>dismiss</button>
+                        </div>
+                    </div>
+                </div>
+                <div className={clasess.item}>
+                    <span>Latest Activities</span>
+                    <div className={clasess.user}>
+                        <div className={clasess.userInfo}>
+                            <img src='https://huivpizde.com/uploads/posts/2022-12/1670018898_16-huivpizde-com-p-erotika-s-milfami-v-bele-18.jpg' alt=''/>
+                            <p>
+                                <span>Jane Doe </span>
+                                changed their cover picture
+                            </p>
+                        </div>
+                        <span>1 min ago</span>
+                    </div>
+                    <div className={clasess.user}>
+                        <div className={clasess.userInfo}>
+                            <img src='https://huivpizde.com/uploads/posts/2022-12/1670018898_16-huivpizde-com-p-erotika-s-milfami-v-bele-18.jpg' alt=''/>
+                            <p>
+                                <span>Jane Doe </span>
+                                changed their cover picture
+                            </p>
+                        </div>
+                        <span>1 min ago</span>
+                    </div>
+                    <div className={clasess.user}>
+                        <div className={clasess.userInfo}>
+                            <img src='https://huivpizde.com/uploads/posts/2022-12/1670018898_16-huivpizde-com-p-erotika-s-milfami-v-bele-18.jpg' alt=''/>
+                            <p>
+                                <span>Jane Doe </span>
+                                changed their cover picture
+                            </p>
+                        </div>
+                        <span>1 min ago</span>
+                    </div>
+                    <div className={clasess.user}>
+                        <div className={clasess.userInfo}>
+                            <img src='https://huivpizde.com/uploads/posts/2022-12/1670018898_16-huivpizde-com-p-erotika-s-milfami-v-bele-18.jpg' alt=''/>
+                            <p>
+                                <span>Jane Doe </span>
+                                changed their cover picture
+                            </p>
+                        </div>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className={clasess.item}>
+                    <span>Online Friends</span>
+                    <div className={clasess.user}>
+                        <div className={clasess.userInfo}>
+                            <img src='https://huivpizde.com/uploads/posts/2022-12/1670018898_16-huivpizde-com-p-erotika-s-milfami-v-bele-18.jpg' alt=''/>
+                            <div className={clasess.online}/>
+                                <span>Jane Doe </span>
+                        </div>
+                    </div>
+                    <div className={clasess.user}>
+                        <div className={clasess.userInfo}>
+                            <img src='https://huivpizde.com/uploads/posts/2022-12/1670018898_16-huivpizde-com-p-erotika-s-milfami-v-bele-18.jpg' alt=''/>
+                            <div className={clasess.online}/>
+                                <span>Jane Doe </span>
+                        </div>
+                    </div>
+                    <div className={clasess.user}>
+                        <div className={clasess.userInfo}>
+                            <img src='https://huivpizde.com/uploads/posts/2022-12/1670018898_16-huivpizde-com-p-erotika-s-milfami-v-bele-18.jpg' alt=''/>
+                            <div className={clasess.online}/>
+                                <span>Jane Doe </span>
+                        </div>
+                    </div>
+                    <div className={clasess.user}>
+                        <div className={clasess.userInfo}>
+                            <img src='https://huivpizde.com/uploads/posts/2022-12/1670018898_16-huivpizde-com-p-erotika-s-milfami-v-bele-18.jpg' alt=''/>
+                            <div className={clasess.online}/>
+                                <span>Jane Doe </span>
+                        </div>
+                    </div>
+                    <div className={clasess.user}>
+                        <div className={clasess.userInfo}>
+                            <img src='https://huivpizde.com/uploads/posts/2022-12/1670018898_16-huivpizde-com-p-erotika-s-milfami-v-bele-18.jpg' alt=''/>
+                            <div className={clasess.online}/>
+                                <span>Jane Doe </span>
+                        </div>
+                    </div>
+                    <div className={clasess.user}>
+                        <div className={clasess.userInfo}>
+                            <img src='https://huivpizde.com/uploads/posts/2022-12/1670018898_16-huivpizde-com-p-erotika-s-milfami-v-bele-18.jpg' alt=''/>
+                            <div className={clasess.online}/>
+                                <span>Jane Doe </span>
+                        </div>
+                    </div>
+                    <div className={clasess.user}>
+                        <div className={clasess.userInfo}>
+                            <img src='https://huivpizde.com/uploads/posts/2022-12/1670018898_16-huivpizde-com-p-erotika-s-milfami-v-bele-18.jpg' alt=''/>
+                            <div className={clasess.online}/>
+                                <span>Jane Doe </span>
+                        </div>
+                    </div>
+                    <div className={clasess.user}>
+                        <div className={clasess.userInfo}>
+                            <img src='https://huivpizde.com/uploads/posts/2022-12/1670018898_16-huivpizde-com-p-erotika-s-milfami-v-bele-18.jpg' alt=''/>
+                            <div className={clasess.online}/>
+                                <span>Jane Doe </span>
+                        </div>
+                    </div>
+                    <div className={clasess.user}>
+                        <div className={clasess.userInfo}>
+                            <img src='https://huivpizde.com/uploads/posts/2022-12/1670018898_16-huivpizde-com-p-erotika-s-milfami-v-bele-18.jpg' alt=''/>
+                            <div className={clasess.online}/>
+                                <span>Jane Doe </span>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
     )
 }
