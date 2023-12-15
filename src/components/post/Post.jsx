920f3c37c9ac6a132cc3bc0c86dc81ba7e4f75a6
Nikita Balashov <noWorkingEmail@mail.ru>
2023-12-15 13:56:59 +0500

    карточка публикаций

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,48 @@
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import './styles.scss'
+import { Link } from 'react-router-dom';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import FavoriteOutlinedIcon from '@mui/icons-material/FavoriteOutlined';
+import TextsmsOutlinedIcon from '@mui/icons-material/TextsmsOutlined';
+import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined';
+
+export default function({data}){
+    const liked = false;
+    return(
+        <div className='post'>
+            <div className="container">
+                <div className="user">
+                    <div className="userInfo">
+                        <img src={data.profile_picture} />
+                        <div className="details">
+                            <Link to={`/profile/${data.userId}`} style={{textDecoration: 'none', color: 'inherit'}}>
+                                <span className='name'>{data.name}</span>
+                            </Link>
+                            <span className='date'>1 min ago</span>
+                        </div>
+                        
+                    </div>
+                    <MoreHorizIcon/>
+                </div>
+                <div className="content">
+                    <p>{data.desctiption}</p>
+                    <div className='img'>
+                        <img src={data.img} />
+                    </div>
+                    
+                </div>
+                <div className="info">
+                    <div className="item">
+                    { liked ? <FavoriteOutlinedIcon/> : <FavoriteBorderOutlinedIcon/> } 12 Likes
+                    </div>
+                    <div className="item">
+                        <TextsmsOutlinedIcon/>12 Comments
+                    </div>
+                    <div className="item">
+                        <ShareOutlinedIcon/>Share
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
